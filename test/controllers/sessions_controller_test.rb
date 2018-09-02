f1472348dc2afa1920b447c9a7f896d0795c1bfc
EC2 Default User <ec2-user@ip-172-31-8-225.us-east-2.compute.internal>
2018-09-02 21:39:44 +0000

    Implement basic login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get login_path
     assert_response :success
