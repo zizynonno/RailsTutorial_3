87d1e14a6a7d32d87d2ae643622c3595a8e64758
EC2 Default User <ec2-user@ip-172-31-8-225.us-east-2.compute.internal>
2018-09-03 09:49:24 +0000

    add signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  post  '/signup',  to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   get  '/login',   to: 'sessions#new' #new->新しいセッションのページ
   post  '/login',   to: 'sessions#create' #create->新しいセッションの作成
   delete  '/logout',   to: 'sessions#destroy'
