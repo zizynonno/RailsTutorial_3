87469b9493c392e0e6a35b87ff6280376d15e3fe
EC2 Default User <ec2-user@ip-172-31-8-225.us-east-2.compute.internal>
2018-10-21 06:55:18 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'fternoon-shelf-62821.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
